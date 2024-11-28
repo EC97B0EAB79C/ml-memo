cd4ef8e203384450dc3518137849e771f0b10f8c
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2024-11-28 14:58:14 +0900

    Updated README

--- tools/model_mod/README.md
+++ tools/model_mod/README.md
@@ -2,7 +2,10 @@
 
 Modify models for certain purpose.
 
-## [ONNX](./onnx_mod.py)
+## ONNX
+### [`onnx_mod`](./onnx_mod.py)
+Removes unused input layers, `Dropout` nodes, and change OP set.
+
 Usage:
 ```bash
 ./onnx_mod.py --help
@@ -14,3 +17,21 @@ options:
   -o, --output OUTPUT  Output file name
   --debug
 ```
+
+### [`onnx_rm_node`](./onnx_rm_node.py)
+Usage:
+```bash
+./onnx_rm_node.py -h
+usage: onnx_rm_node.py [-h] -m MODEL [-o OUTPUT] [--nodes NODES [NODES ...]] [--layer LAYER [LAYER ...]]
+
+options:
+  -h, --help            show this help message and exit
+  -m, --model MODEL     Model to create data for
+  -o, --output OUTPUT   Output file name
+  --nodes NODES [NODES ...]
+                        List of nodes to remove
+  --layer LAYER [LAYER ...]
+                        List of output layers to remove
+```
+
+### [`onnx_dim_set`](./onnx_dim_set.py)
--- tools/model_mod/onnx_rm_node.py
+++ tools/model_mod/onnx_rm_node.py
@@ -1,22 +1,42 @@
+#!/usr/bin/env python
+import argparse
+
+##
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-m", "--model", type=str, required=True, help = "Model to create data for"
+)
+parser.add_argument(
+    "-o", "--output", type=str, default = "./modified.onnx", help = "Output file name"
+)
+parser.add_argument(
+    "--nodes", nargs="+", help="List of nodes to remove"
+)
+parser.add_argument(
+    "--layer", nargs="+", help="List of output layers to remove"
+)
+args = parser.parse_args()
+
+##
+# Modify model
 import onnx
 from onnx import helper, version_converter
 from onnx import shape_inference
 
-MODEL = "yolov3-12.onnx"
 RM_NODE = ["yolonms_layer_1/non_max_suppression/NonMaxSuppressionV3", "Cast"]
 RM_OUTPUT = ["yolonms_layer_1/concat_2:0"]
-MODIFIED_MODEL = "./modified.onnx"
 
-model = onnx.load(MODEL)
+model = onnx.load(args.model)
 graph = model.graph
 
-target_node = [node for node in graph.node if node.name in RM_NODE]
+target_node = [node for node in graph.node if node.name in args.nodes]
 for node in target_node:
     graph.node.remove(node)
 
-new_output = [o for o in graph.output if o.name not in RM_OUTPUT]
+new_output = [o for o in graph.output if o.name not in args.layer]
 graph.ClearField('output')
 graph.output.extend(new_output)
 
 
-onnx.save(model, MODIFIED_MODEL)
+onnx.save(model, args.output)
