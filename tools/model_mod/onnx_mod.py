d0879b3bd88251c8490ef964067b687c796894ab
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2024-11-29 15:56:27 +0900

    Reorder model.graph.input

--- tools/model_mod/onnx_mod.py
+++ tools/model_mod/onnx_mod.py
@@ -23,8 +23,12 @@ args = parser.parse_args()
 import onnx
 from onnx import helper, version_converter
 from onnx import shape_inference
+import onnxruntime as ort
 
 model = onnx.load(args.model)
+graph = model.graph
+session = ort.InferenceSession(args.model)
+
 if args.debug:
     print("===Before===")
     for input in model.graph.input:
@@ -34,7 +38,6 @@ if args.debug:
 
 
 # Remove Dropout Layers
-graph = model.graph
 dropout_nodes = [node for node in graph.node if node.op_type == "Dropout"]
 if len(dropout_nodes):
     print("Removing `Dropout` nodes")
@@ -77,10 +80,23 @@ for node in graph.node:
         used_inputs.add(input_name)
 new_inputs = [input for input in graph.input if input.name in used_inputs]
 
-if original_inputs != used_inputs:
+if sorted([n for n in original_inputs]) != sorted([input.name for input in new_inputs]):
     print("Remove unnecessary model.graph.input")
     graph.ClearField('input')
     graph.input.extend(new_inputs)
+
+
+# Change model.graph.input order
+ort_input_names = [input.name for input in session.get_inputs()]
+input_mapping = {input.name: input for input in graph.input}
+
+reordered_inputs = [input_mapping[name] for name in ort_input_names if name in input_mapping]
+reordered_inputs += [input for input in graph.input if input.name not in ort_input_names]
+
+if [input.name for input in reordered_inputs] != [input.name for input in model.graph.input]:
+    print("Input order has changed.")
+    graph.ClearField('input')
+    graph.input.extend(reordered_inputs)
 
 
 # Change opset
@@ -88,6 +104,7 @@ if args.op:
     print("Change OP set")
     model = version_converter.convert_version(model, args.op)
 
+# Save model
 onnx.save(model, args.output)
 
 
